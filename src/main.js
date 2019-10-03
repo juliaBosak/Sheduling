8b7adbd7b1452848e779518c6a49fbe023a3174f
juliabosak <1kn15b.bosak@gmail.com>
2019-10-03 11:12:11 +0300

    add all files

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-import Vuelidate from 'vuelidate'
-Vue.use(Vuelidate)
-Vue.config.productionTip = false;
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app');
